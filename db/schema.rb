e8ec6e71b7fbac714cfecc9a35fc83b3bfc8876a
lanbau <irvinln@gmail.com>
2016-01-23 07:09:40 +0800

    Initial Commit
    End result of getting started with rails tutorial.

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160122160709) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "company", force: :cascade do |t|
+    t.text    "name",               null: false
+    t.integer "age",                null: false
+    t.string  "address", limit: 50
+    t.float   "salary"
+  end
+
+  create_table "persons", id: false, force: :cascade do |t|
+    t.integer "personid"
+    t.string  "lastname",  limit: 255
+    t.string  "firstname", limit: 255
+    t.string  "address",   limit: 255
+    t.string  "city",      limit: 255
+  end
+
+  add_foreign_key "comments", "articles"
+end
